6c429e42972d059584b98f51948495bcebd9e912
Michael Georgoulopoulos <mgeorgoulopoulos@gmail.com>
2017-05-09 23:33:39 +0300

    removed useless "running" var

--- source/main.c
+++ source/main.c
@@ -1,38 +1,4 @@
-/*
-===========================================================
-                Sony PlayStation 1 Source Code
-===========================================================
-                         FONT EXAMPLE
-Displays text on the screen using the built in GPU routines
------------------------------------------------------------
-
-    Developer / Programmer..............: SCEI & PSXDEV.net
-    Software Ddevelopment Kit...........: PSY-Q
-    Last Release........................: 30/APRIL/2014
-
-    Original code by SCEI | Edited by PSXDEV.net
-
-	NOTE: This example uses double buffering! I may go
-	ahead and remove the double buffering to make the code
-	much more simplistic in the future.
-
-    All PlayStation 1 games are written in C. If you
-	don't know how to program in C, you should follow a
-	book or online tutorial and then take a look at the
-	PSX C code. Above all, the PSX is an excellent system
-	to program on and work with and will not dissapoint you.
-
-    If you need help, read LIBOVR46.PDF in PSYQ/DOCS
-    also join our IRC channel on EFNET #psxdev
-
-  Copyright (C) 1994,1995 by Sony Computer Entertainment Inc.
-                     All Rights Reserved.
-
-   Sony Computer Entertainment Inc. Development Department
-
-                    http://psxdev.net/
-
------------------------------------------------------------*/
+/* OUF ka8arise o topos... */
 
 #include "helper.h"
 
@@ -40,7 +6,6 @@ RENDERABLE triangle;
 
 int main()
 {
-    u_char running = 1;
     SVECTOR x[3];
     u_short padd;
     VECTOR cameraPos = {0,0,1000};
@@ -68,7 +33,7 @@ int main()
 	init_renderable(&triangle, &t, 1, x, sizeof(SVECTOR));
 
 	globals->frameIdx = 0;
-	while (running) // draw and display forever
+	while (1) // draw and display forever
 	{
 	    globals->cdb = &globals->db[globals->frameIdx % MAX_BUFFERS];
         padd = PadRead(1);
