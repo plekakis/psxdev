ce1ef9cd7620b2eb60f28fb283988a22132da923
Pantelis Lekakis <p.lekakis@icloud.com>
2018-07-28 20:10:03 +0100

    Removed the various bat files

--- projects/sandbox/package and run.bat
+++
@@ -1,7 +0,0 @@
-pushd cdrom\root
-mkpsxiso demo.xml -y
-if %errorlevel% neq 0 pause & exit /b %errorlevel%
-popd
-
-no$psx.exe cdrom/demo.cue
-pause
--- projects/sandbox/package and run_psx.bat
+++
@@ -1,7 +0,0 @@
-pushd cdrom\root
-mkpsxiso demo.xml -y
-if %errorlevel% neq 0 pause & exit /b %errorlevel%
-popd
-
-catflap run cdrom/root/main.exe
-pause
